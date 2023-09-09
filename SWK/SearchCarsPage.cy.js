a609b8724e6adca6148f30a54ffe27c6cf75fa40
sajjad <sajjad.khalid@gmail.com>
2023-09-09 16:10:06 -0400

    added SWK

---
+++ SWK/SearchCarsPage.cy.js
@@ -0,0 +1,21 @@
+
+
+    it('ESPN', () => {
+        cy.visit('https://www.saucedemo.com/')
+
+      /*
+        cy.visit('https://www.espn.com/')
+        cy.contains('NFL').click({force:true})
+        cy.url().should('include', '/nfl/')
+        
+        cy.get('.contentItem__titleWrapper h2')
+        .should('be.visible')
+        .and('contain', 'Our annual NFL Rank')
+        
+*/
+        })
+
+       
+        
+  //  })
+
